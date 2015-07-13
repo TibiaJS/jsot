0fe20ada8f8e11d63d1b86947e6be1474b7123f3
Ranieri Althoff <ranisalt@gmail.com>
2015-07-13 14:59:14 -0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var gulp = require('gulp'),
+    mocha = require('gulp-mocha');
+
+gulp.task('default', function() {
+    return gulp.src(['test/*.js'], { read: false })
+        .pipe(mocha({
+            reporter: 'spec'
+        }));
+});
