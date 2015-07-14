99cbc0db49e21db30d55092d5159f5ec3189b590
Ranieri Althoff <ranisalt@gmail.com>
2015-07-14 04:19:12 -0300

    Create database handler

---
+++ src/database.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var mysql = require('mysql');
+
+module.exports = function(host, user, password, database) {
+    return mysql.createPool({
+        connectionLimit: 100,
+        host: host,
+        user: user,
+        password: password,
+        database: database,
+        queryFormat: function(query, values) {
+            if (!values) return query;
+            return query.replace(/\:(\w+)/g, function(txt, key) {
+                if (values.hasOwnProperty(key)) {
+                    return this.escape(values[key]);
+                }
+                return txt;
+            }.bind(this));
+        }
+    });
+
+};
